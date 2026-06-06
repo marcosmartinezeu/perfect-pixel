d0a4e9111505a04237fa9ac1127d8e65c662adb1
Marcos Martínez <marcos.martinez.muniz@gmail.com>
2026-06-06 10:00:30 +0200

    fix: some fixes from PR

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from './app'
 import { env } from './shared/infrastructure/config/env'
-import { prisma } from '@repo/database'
 
 async function bootstrap(): Promise<void> {
     const app = await createApp()
-
-    console.log('count', await prisma.image.count());
 
     await app.listen({
         port: env.apiPort,
